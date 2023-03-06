4c3cb51e0288f07f190abc5581328bd92489d987
Adewuyi Taofeeq Olamilekan <36686152+harmlessprince@users.noreply.github.com>
2023-03-06 14:45:21 +0100

    updatde credit check creating (#19)
    Co-authored-by: harmlessprince <realolamilekan@gmail>

--- app/Models/CreditCheckerVerification.php
+++ app/Models/CreditCheckerVerification.php
@@ -15,7 +15,7 @@ class CreditCheckerVerification extends Model
     const FAILED = 'FAILED';
     const STATUSES = [self::PENDING, self::PASSED, self::FAILED];
 
-
+    protected $with = ['product', 'repaymentDuration', 'repaymentCycle', 'downPaymentRate'];
     public function customer()
     {
         return $this->belongsTo(Customer::class, 'customer_id');
@@ -29,5 +29,19 @@ class CreditCheckerVerification extends Model
     public function product()
     {
         return $this->belongsTo(BnplVendorProduct::class, 'bnpl_vendor_product_id');
+    }
+
+    public function repaymentDuration()
+    {
+        return $this->belongsTo(RepaymentDuration::class);
+    }
+
+    public function repaymentCycle()
+    {
+        return $this->belongsTo(RepaymentCycle::class);
+    }
+    public function downPaymentRate()
+    {
+        return $this->belongsTo(DownPaymentRate::class, 'down_payment_rate_id');
     }
 }
