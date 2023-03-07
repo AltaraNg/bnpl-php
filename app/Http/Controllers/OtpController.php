2a528509ccbe0f19380b564ec4cf7b73610f0df5
harmlessprince <realolamilekan@gmail>
2023-03-07 17:26:56 +0100

    Otp Senidng

---
+++ app/Http/Controllers/OtpController.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Services\OtpService;
+use App\Services\SendSmsService;
+use Illuminate\Http\Request;
+
+class OtpController extends Controller
+{
+
+    public function __construct(public readonly SendSmsService $sendSmsService, public readonly OtpService $otpService)
+    {
+    }
+    public function generateOtp(Request $request)
+    {
+        $this->validate($request, [
+            'phone_number' => ['required', 'string', 'max:11']
+        ]);
+        $response  = $this->otpService->createOtp($request->input('phone_number'));
+        if ($response->status == false) {
+            return $this->respondError($response->message, 400);
+        }
+        $message = $response->otp;
+        $response  = $this->sendSmsService->sendMessage($request->input('phone_number'), $message);
+        if ($response === true) {
+            return $this->respondSuccess([], 'Otp Sent Successfully');
+        }
+        return $this->respondError('Otp failed to send', 400);
+    }
+
+    public function validateOtp(Request $request)
+    {
+        $this->validate($request, [
+            'phone_number' => ['required', 'string', 'max:11'],
+            'otp' => ['required', 'string', 'min:6'],
+        ]);
+
+        $response  = $this->otpService->validate($request->input('phone_number'), $request->input('otp'));
+
+        if ($response->status === true) {
+            return $this->respondSuccess([], $response->message);
+        }
+        return $this->respondError($response->message, 400);
+    }
+}
---
+++ app/Services/OtpService.php
@@ -0,0 +1,111 @@
+<?php
+
+namespace App\Services;
+
+use App\Models\Otp;
+use Carbon\Carbon;
+use Illuminate\Support\Facades\DB;
+
+class OtpService
+{
+
+    private $validity = 30;
+    private $deleteOldOtps = 30;
+
+    public function createOtp($identifier)
+    {
+       
+        $this->deleteOldOtps();
+        $otp = Otp::where('identifier', $identifier)->first();
+        if ($otp == null) {
+            $otp = Otp::create([
+                'identifier' => $identifier,
+                'token' => $this->generateNumericOTP(6),
+                'validity' => $this->validity,
+                'generated_at' => Carbon::now(),
+            ]);
+        } else {
+            if (!$otp->isExpired() && !request('regenerate')) {
+                return (object)[
+                    'status' => false,
+                    'message' => 'Please check your message, you have an otp that expires in: ' . $otp->expiresIn(),
+                ];
+            }
+            $otp->update([
+                'identifier' => $identifier,
+                'token' => $this->generateNumericOTP(6),
+                'validity' => $this->validity,
+                'generated_at' => Carbon::now(),
+            ]);
+        }
+        return (object)[
+            'status' => true,
+            'otp' => $otp->token,
+            'message' => "OTP generated",
+            'expires_in' => $otp->expiresIn(),
+        ];
+    }
+
+
+    public function validate(string $identifier, string $token): object
+    {
+        $otp = Otp::where('identifier', $identifier)->first();
+
+        if (!$otp) {
+            return (object)[
+                'status' => false,
+                'message' =>'Otp not found',
+            ];
+        }
+
+        if ($otp->isExpired()) {
+            return (object)[
+                'status' => false,
+                'message' => 'Invalid otp, please generate a new one',
+            ];
+        }
+
+        if ($otp->token == $token) {
+            return (object)[
+                'status' => true,
+                'message' => 'OTP is valid',
+            ];
+        }
+        return (object)[
+            'status' => false,
+            'message' => 'OTP does not match',
+        ];
+    }
+
+
+    public function deleteOldOtps(): bool
+    {
+        // dd($this->deleteOldOtps);
+        return Otp::where('expired', true)
+            ->orWhere('created_at', '<', Carbon::now()->subMinutes($this->deleteOldOtps))
+            ->delete();
+    }
+
+    // Function to generate OTP
+    public function generateNumericOTP(int $n): string
+    {
+
+        // Take a generator string which consist of
+        // all numeric digits
+        $generator = "1357902468";
+
+        // Iterate for n-times and pick a single character
+        // from generator and append it to $result
+
+        // Logic for generating a random character from generator
+        //     ---generate a random number
+        //     ---take modulus of same with length of generator (say i)
+        //     ---append the character at place (i) from generator to result
+        $result = "";
+        for ($i = 1; $i <= $n; $i++) {
+            $result .= substr($generator, (rand() % (strlen($generator))), 1);
+        }
+        // Return result
+        return $result;
+    }
+}
