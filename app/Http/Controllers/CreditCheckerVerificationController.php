bd9145bcfc668ad329c81031e0e44124975c398e
harmlessprince <realolamilekan@gmail>
2023-03-09 10:15:04 +0100

    logging mail issue

--- app/Http/Controllers/CreditCheckerVerificationController.php
+++ app/Http/Controllers/CreditCheckerVerificationController.php
@@ -13,6 +13,7 @@ use App\Repositories\Eloquent\Repository\CustomerRepository;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Notification;
 use App\Models\User;
+use Illuminate\Support\Facades\Log;
 
 class CreditCheckerVerificationController extends Controller
 {
@@ -25,64 +26,72 @@ class CreditCheckerVerificationController extends Controller
 
     public function store(OrderRequest $request)
     {
+        try {
+            $customer = $this->customerRepository->findById($request->input('customer_id'));
 
-        $customer = $this->customerRepository->findById($request->input('customer_id'));
+            $guarantors  = GuarantorDto::fromOrderApiRequest($request);
+            foreach ($guarantors as $key => $guarantor) {
+                $guarantorDto  = GuarantorDto::fromSelf($guarantor);
+                Guarantor::query()->updateOrCreate(
+                    [
+                        'customer_id' => $guarantorDto->customer_id,
+                        'phone_number' => $guarantorDto->phone_number
+                    ],
+                    [
+                        'first_name' => $guarantorDto->first_name,
+                        'last_name' => $guarantorDto->last_name,
+                        'home_address' => $guarantorDto->home_address,
+                    ]
+                );
+            }
+            /** @var User $vendor */
+            $vendor = auth()->user();
 
-        $guarantors  = GuarantorDto::fromOrderApiRequest($request);
-        foreach ($guarantors as $key => $guarantor) {
-            $guarantorDto  = GuarantorDto::fromSelf($guarantor);
-            Guarantor::query()->updateOrCreate(
+            $product = BnplVendorProduct::query()->updateOrCreate(
                 [
-                    'customer_id' => $guarantorDto->customer_id,
-                    'phone_number' => $guarantorDto->phone_number
+                    'name' => $request->product_name,
+                    'vendor_id' => $vendor->id,
                 ],
-                [
-                    'first_name' => $guarantorDto->first_name,
-                    'last_name' => $guarantorDto->last_name,
-                    'home_address' => $guarantorDto->home_address,
-                ]
+                ['price' => $request->product_price]
             );
+            if ($customer->creditCheckerVerifications()->where('status', CreditCheckerVerification::PENDING)->exists()) {
+                $creditCheckerVerification = $customer->latestCreditCheckerVerifications()->where('status', CreditCheckerVerification::PENDING)->first();
+            } else {
+                $creditCheckerVerification = CreditCheckerVerification::create([
+                    'customer_id' => $request->input('customer_id'),
+                    'initiated_by' => request()->user()->id,
+                    'bnpl_vendor_product_id' => $product->id,
+                    'repayment_duration_id' => $request->input('repayment_duration_id'),
+                    'repayment_cycle_id' => $request->input('repayment_cycle_id'),
+                    'down_payment_rate_id' => $request->input('down_payment_rate_id')
+                ]);
+            }
+            Notification::route('mail', config('app.credit_checker_mail'))->notify(new PendingCreditCheckNotification($customer, $vendor, $product, $creditCheckerVerification));
+            return $this->respondSuccess(['credit_check_verification' =>  $creditCheckerVerification], 'Credit check initiated and notification sent');
+        } catch (\Throwable $th) {
+            Log::error($th);
+            return $this->respondError('An error ocurred while try to initiate credit check');
         }
-        /** @var User $vendor */
-        $vendor = auth()->user();
-
-        $product = BnplVendorProduct::query()->updateOrCreate(
-            [
-                'name' => $request->product_name,
-                'vendor_id' => $vendor->id,
-            ],
-            ['price' => $request->product_price]
-        );
-        if ($customer->creditCheckerVerifications()->where('status', CreditCheckerVerification::PENDING)->exists()) {
-            $creditCheckerVerification = $customer->latestCreditCheckerVerifications()->where('status', CreditCheckerVerification::PENDING)->first();
-        } else {
-            $creditCheckerVerification = CreditCheckerVerification::create([
-                'customer_id' => $request->input('customer_id'),
-                'initiated_by' => request()->user()->id,
-                'bnpl_vendor_product_id' => $product->id,
-                'repayment_duration_id' => $request->input('repayment_duration_id'),
-                'repayment_cycle_id' => $request->input('repayment_cycle_id'),
-                'down_payment_rate_id' => $request->input('down_payment_rate_id')
-            ]);
-        }
-        Notification::route('mail', config('app.credit_checker_mail'))->notify(new PendingCreditCheckNotification($customer, $vendor, $product, $creditCheckerVerification));
-        return $this->respondSuccess(['credit_check_verification' =>  $creditCheckerVerification], 'Credit check initiated and notification sent');
     }
 
     public function verifyCreditCheck(CreditCheckerVerification $creditCheckerVerification)
     {
-        /** @var User $vendor */
-        $vendor = auth()->user();
-        if ($creditCheckerVerification->status === CreditCheckerVerification::PENDING) {
-            Notification::route('mail', config('app.credit_checker_mail'))->notify(new PendingCreditCheckNotification($creditCheckerVerification->customer, $creditCheckerVerification->vendor, $creditCheckerVerification->product, $creditCheckerVerification));
-            return $this->respondSuccess(['status' => CreditCheckerVerification::PENDING], 'Credit check still pending, please check again in 5 minutes');
+        try {
+            /** @var User $vendor */
+            $vendor = auth()->user();
+            if ($creditCheckerVerification->status === CreditCheckerVerification::PENDING) {
+                Notification::route('mail', config('app.credit_checker_mail'))->notify(new PendingCreditCheckNotification($creditCheckerVerification->customer, $creditCheckerVerification->vendor, $creditCheckerVerification->product, $creditCheckerVerification));
+                return $this->respondSuccess(['status' => CreditCheckerVerification::PENDING], 'Credit check still pending, please check again in 5 minutes');
+            }
+            if ($creditCheckerVerification->status === CreditCheckerVerification::FAILED) {
+                return $this->respondSuccess(['status' => CreditCheckerVerification::FAILED], 'Credit Check failed');
+            }
+            if ($creditCheckerVerification->status === CreditCheckerVerification::PASSED) {
+                return $this->respondSuccess(['status' => CreditCheckerVerification::PASSED], 'Credit check has been passed');
+            }
+        } catch (\Throwable $th) {
+            Log::error($th);
+            return $this->respondError('An error ocurred while try to verify credit check status');
         }
-        if ($creditCheckerVerification->status === CreditCheckerVerification::FAILED) {
-            return $this->respondSuccess(['status' => CreditCheckerVerification::FAILED], 'Credit Check failed');
-        }
-        if ($creditCheckerVerification->status === CreditCheckerVerification::PASSED) {
-            return $this->respondSuccess(['status' => CreditCheckerVerification::PASSED], 'Credit check has been passed');
-        }
-       
     }
 }
