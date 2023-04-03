d5ff47ee7b1748d6a7081728b677e23bdc71ffe8
Adewuyi Taofeeq Olamilekan <36686152+harmlessprince@users.noreply.github.com>
2023-04-03 09:57:51 +0100

    Fixed Customer Listing Issue (#35)
    * sending text and mail to auth user
    
    * fixed
    
    * fixed error
    
    * removed dd
    
    ---------
    
    Co-authored-by: harmlessprince <realolamilekan@gmail>

--- app/Repositories/Eloquent/Repository/CustomerRepository.php
+++ app/Repositories/Eloquent/Repository/CustomerRepository.php
@@ -28,7 +28,7 @@ class CustomerRepository extends BaseRepository
         return $this->model::query()->when(request('telephone'), function ($query) {
             $query->where('telephone', 'LIKE', '%' . request('telephone') . '%');
         })->with('orders')->latest('created_at')->when(request('telephone') == null, function ($query) use ($vendor_id) {
-            $query->where('user_id', $vendor_id)->orWhereHas('bnplCustomers', function ($query) use ($vendor_id)
+            $query->where('user_id', $vendor_id)->orWhereHas('merchants', function ($query) use ($vendor_id)
             {
                 $query->where('user_id', $vendor_id);
             });
