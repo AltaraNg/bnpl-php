76e904ace12c087b2093a92253c67d4201a8026f
harmlessprince <realolamilekan@gmail>
2023-04-05 14:05:09 +0100

    ordering commissions correctly

--- app/Http/Controllers/CommissionController.php
+++ app/Http/Controllers/CommissionController.php
@@ -10,7 +10,7 @@ class CommissionController extends Controller
 {
     public function index()
     {
-        $commissions =  MerchantCommission::query()->latest('created_at')->where('merchant_id', request()->user()->id)
+        $commissions =  MerchantCommission::query()->orderBy('id', 'DESC')->where('merchant_id', request()->user()->id)
         ->with('product', 'commission', 'order')
         ->paginate(request('per_page', 15));
         return $this->respondSuccess(['commissions' => $commissions], 'Fetched commissions');
