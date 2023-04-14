cec6268988bec421745fdd33a046a402ca717473
Adewuyi Taofeeq Olamilekan <36686152+harmlessprince@users.noreply.github.com>
2023-04-14 18:11:50 +0100

    increased file upload size (#43)
    Co-authored-by: harmlessprince <realolamilekan@gmail>

--- app/Http/Controllers/FileController.php
+++ app/Http/Controllers/FileController.php
@@ -13,7 +13,7 @@ class FileController extends Controller
     public function uploadSingleFile(Request $request)
     {
         $this->validate($request, [
-            'file' => ['required', 'file', 'max:512'],
+            'file' => ['required', 'file', 'size:5000'],
             'name' => ['required', 'string'],
         ]);
 
@@ -30,7 +30,7 @@ class FileController extends Controller
     {
         $this->validate($request, [
             'documents' => ['required', 'array'],
-            'documents.*.file' => ['required', 'file', 'max:512'],
+            'documents.*.file' => ['required', 'file', 'size:5000'],
             'documents.*.name' => ['required', 'string'],
         ]);
 
