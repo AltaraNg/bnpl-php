6de0833cb987ef3cd739b9b20428a5d0967282b0
harmlessprince <realolamilekan@gmail>
2023-04-17 11:03:30 +0100

    throw actual error

--- app/Http/Controllers/FileController.php
+++ app/Http/Controllers/FileController.php
@@ -63,8 +63,8 @@ class FileController extends Controller
             }
             return Storage::disk('s3')->url($pathToImage);
         } catch (\Throwable $th) {
-            Log::error($th);
-            throw new Error('Error occurred while uploading the file');
+           
+            throw new Error($th->getMessage());
         }
     }
 }
